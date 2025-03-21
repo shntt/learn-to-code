4f0ac3964dc1d2ddd228f15230006d1c3d2932de
shntt <highbridge822@me.com>
2025-03-21 22:57:56 +0900

    solved 07-valid-anagram

--- practice/06-contains-duplicate.js
+++ practice/06-contains-duplicate.js
@@ -6,37 +6,30 @@
 
 let nums = [];
 
-// 1回目に書いたコード
-// 計算量 O(n^2)?
-//
-/* function containsDuplicate(nums) {
+// 第1案
+// 2乗ループで計算量 最大の場合、O(n^2)に近い？
+function containsDuplicate1(nums) {
     for (let i = 0; i < nums.length - 1; i++) {
         if (nums.includes(nums[i], i + 1)) {
             return true;
         }
     }
     return false 
-} */
+}
 
-// 改善案
-// ハッシュマップを使うと、O(1)で検索できる
-// ハッシュマップの作成の計算量は、O(n)
-// したがって、1回目のコードより計算量を減らせるはず
-//
-function containsDuplicate(nums) {
-    let hashMap = {};
+// 第2案
+// Mapの作成の計算量はO(n)。検索も最大O(N)で可能
+function containsDuplicate2(nums) {
+    let map = new Map;
 
-    // ハッシュマップを作成する
-    // キーにnumsの要素、バリューにそれぞれのインデックスを登録する
-    // ハッシュマップでは、すでに同じキーが存在する場合、バリューは上書きされる
+    // Mapを作成する(重複が存在する場合、valueは上書きされる)
     for (let i = 0; i < nums.length; i++) {
-        hashMap[nums[i]] = i;
+        map.set(nums[i], i);
     }
 
-    // hashMapのバリューとnumsのインデックスを0から比較する
-    // それらが一致しない＝重複が存在するため、trueを返す
+    // hashMapのvalueとnumsのindexが一致しない ＝ 重複が存在するため、trueを返す
     for (let i = 0; i < nums.length; i++) {
-        if (hashMap[nums[i]] !== i) {
+        if (map.get(nums[i]) !== i) {
             return true;
         }
     }
@@ -44,8 +37,17 @@ function containsDuplicate(nums) {
     return false;
 }
 
-// nums = [1,2,3,1];               // true
+// 第3案
+// Setを使って重複する要素を削除して、元の配列と長さを比較する
+function containsDuplicate3(nums) {
+    const newNums = new Set(nums);
+    return nums.length !== newNums.size;
+}
+// Mapと違い、valueを持たないため、さらに計算量が少なく済むのか？
+// メソッドを使うときは、そのメソッドの挙動・計算量を理解したほうがよさそう
+
+nums = [1,2,3,1];               // true
 // nums = [1,2,3,4];               // false
 // nums = [1,1,1,3,3,4,3,2,4,2];   // true
 
-containsDuplicate(nums)
+containsDuplicate3(nums);
